9a9b1494a5d75049cc8bb1cf95183a006be41c3b
bigbigtime <409019683@qq.com>
2019-08-11 23:28:37 +0800

    CSS3选择器

---
+++ js.js
@@ -0,0 +1,44 @@
+let object = [
+    {
+        title: 'CSS3 选择器——基本选择器',
+        data: [
+            {  text: '通配符（*）', id: 'basic1', url: 'basic1.html', height: '415px' },
+            {  text: '选择器（Element）', id: 'basic2', url: 'basic2.html', height: '435px' },
+            {  text: '类选择器（.className）', id: 'basic3', url: 'basic3.html', height: '435px' },
+            {  text: 'id选择器（#ID）', id: 'basic4', url: 'basic4.html', height: '435px' },
+            {  text: '后代选择器（E F）', id: 'basic5', url: 'basic5.html', height: '435px' },
+            {  text: '子元素选择器（E > F）', id: 'basic6', url: 'basic6.html', height: '435px' },
+            {  text: '相邻兄弟元素选择器(E + F)', id: 'basic7', url: 'basic7.html', height: '475px' },
+            {  text: '通用兄弟选择器（E 〜 F）', id: 'basic8', url: 'basic8.html', height: '455px' },
+            {  text: '群组选择器', id: 'basic9', url: 'basic9.html', height: '455px' }
+        ]
+    },
+    {
+        title: 'CSS3 选择器——属性选择器',
+        data: [
+            {  text: 'E[attr]', id: 'type1', url: 'type1.html', height: '435px' },
+            {  text: 'E[attr="value"]', id: 'type2', url: 'type2.html', height: '435px' },
+            {  text: 'E[attr~="value"]', id: 'type3', url: 'type3.html', height: '475px' },
+            {  text: 'E[attr^="value"]', id: 'type4', url: 'type4.html', height: '455px' },
+            {  text: 'E[attr$="value"]', id: 'type5', url: 'type5.html', height: '435px' },
+            {  text: 'E[attr*="value"]', id: 'type6', url: 'type6.html', height: '455px' },
+            {  text: 'E[attr|="value"]', id: 'type7', url: 'type7.html', height: '435px' }
+        ]
+    },
+    {
+        title: 'CSS3 选择器——伪类选择器',
+        data: [
+            {  text: ':fist-child', id: 'nth1', url: 'nth1.html', height: '140px' },
+            {  text: ':last-child', id: 'nth2', url: 'nth2.html', height: '140px' },
+            {  text: ':nth-child()', id: 'nth3', url: 'nth3.html', height: '1975px' },
+            {  text: ':nth-last-child()', id: 'nth4', url: 'nth4.html', height: '725px' },
+            {  text: ':nth-of-nth()', id: 'nth5', url: 'nth5.html', height: '295px' },
+            {  text: ':nth-last-of-nth()', id: 'nth6', url: 'nth6.html', height: '160px' },
+            {  text: ':first-of-nth', id: 'nth7', url: 'nth7.html', height: '160px' },
+            {  text: ':last-of-nth', id: 'nth8', url: 'nth8.html', height: '160px' },
+            {  text: ':only-child', id: 'nth9', url: 'nth9.html', height: '485px' },
+            {  text: ':only-of-nth', id: 'nth10', url: 'nth10.html', height: '455px' },
+            {  text: ':empty', id: 'nth11', url: 'nth11.html', height: '415px' }
+        ]
+    }
+]
